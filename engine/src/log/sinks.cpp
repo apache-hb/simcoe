08770858294d2603854fbda8cac7280274bd103a
Elliot <35050275+apache-hb@users.noreply.github.com>
2023-11-08 23:33:33 -0500

    change default log path

--- engine/src/log/sinks.cpp
+++ engine/src/log/sinks.cpp
@@ -9,7 +9,7 @@ using namespace simcoe;
 using namespace simcoe::log;
 
 config::ConfigValue<bool> cfgLogColour("logging/console", "colour", "enable coloured console output", true, config::eDynamic);
-config::ConfigValue<std::string> cfgLogPath("logging/file", "path", "path to log file", "log.txt");
+config::ConfigValue<std::string> cfgLogPath("logging/file", "path", "path to log file", "engine.log");
 
 bool hasColourSupport() {
     DWORD dwMode = 0;
